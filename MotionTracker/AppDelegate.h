0057818d810055c1ccd99989e494771323338b9a
flickmarcel <marcel.flick@mapcase.de>
2013-12-29 10:30:04 +0100

    Initial Commit

---
+++ MotionTracker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MotionTracker
+//
+//  Created by Marcel Flick on 29.12.13.
+//  Copyright (c) 2013 Marcel Flick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MotionTracker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MotionTracker
+//
+//  Created by Marcel Flick on 29.12.13.
+//  Copyright (c) 2013 Marcel Flick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
